488130ccc4bc15f7cb6537e5e496345d671a120e
JuanC <JuanC_lopgar97@outlook.com>
2025-02-27 01:32:01 -0600

    First version of python sharp with delegates and events

---
+++ python_sharp.py
@@ -0,0 +1,251 @@
+from typing import Callable,Any,List,Generic,TypeVar,Union
+
+T=TypeVar("T")
+
+class Delegate(Generic[T]):
+    """
+    Delegate is a simple class that represents a collection of Callables. When is being called execute all the callables in its collection.
+
+    Attributes:
+        _callables (list): Stores a collection of Callables.
+    """
+    _callables:list
+
+    def __init__(self,callable:Callable[..., T] | None = None)->None:
+        """
+        Delegate constructor.
+
+        Parameters:
+            callable (Callable[..., T] | None): first callable to be added to the collection of callables.
+
+        Return:
+            None
+        """
+        self._callables = [] 
+
+        if callable is not None:
+            self += callable
+
+
+    def __iadd__(self, value:Callable[..., T])->"Delegate":
+        """
+        Implements the addition of a callable into the callable collection.
+
+        Parameters:
+            value (Callable[..., T]): callable to be added to the collection of callables.
+
+        Return:
+            Delegate: Current instance with the extra callable.
+        """
+        self._callables.append(value)
+        return self
+
+    def __isub__(self, value:Callable[..., T])->"Delegate":
+        """
+        Implements the subtraction of a callable into the callable collection.
+
+        Parameters:
+            value (Callable[..., T]): callable to be subtracted to the collection of callables.
+
+        Return:
+            Delegate: Current instance with without the passed callable.
+        """
+        self._callables.remove(value)
+        return self
+    
+
+    def __call__(self, *args:Any, **kwds:Any)->List[T]:
+        """
+        Allows the Delegate instance to be called as a function.
+
+        Parameters:
+            *args: A variable number of positional arguments that are going to be pass to every callable in the callable collection.
+            **kwds: A variable number of named arguments that are going to be pass to every callable in the callable collection (keyword arguments).
+
+        Return:
+            List[T]: A list of the results of every callable in the callable collection.
+        """
+        results = []
+
+        for callable in self._callables:
+            results.append(callable( *args, **kwds))
+
+        return results
+    
+
+class EventArgs:
+    """
+    EventArgs is a class that represents a object that contains the event arguments (reasons or extra information about why the event was triggered)
+    """
+    pass
+
+
+class CancellableEventArg(EventArgs):
+    """
+    CancellableEventArg is a class that represents an EventArg that implements the posibility of cancelling the upcomming event setting the property Cancel to 'True'.
+
+    Attributes:
+        _cancel (bool): Contains the value if the action advertise by the pre-event should continue or not.
+    """
+    _cancel:bool
+
+    def __init__(self)->None:
+        """
+        CancellableEventArg constructor.
+
+        Return:
+            None
+        """
+        super().__init__()
+        self._cancel = False 
+
+    
+    @property
+    def Cancel(self)->bool:
+        """
+        Gets property value.
+
+        Return:
+            bool: current value.
+        """
+        return self._cancel
+    
+    @Cancel.setter
+    def Cancel(self,value:bool)->None:
+        """
+        Sets a new value.
+
+        Parameters:
+            value (bool): New value to set.
+        """
+        self._cancel = value
+
+
+
+class event:
+    """
+    Event attribute, used to define a managed callback
+
+    Attributes:
+        _fadd (Callable[[Callable[[object,EventArgs], None]], None] | None): function to be used for adding a callable value.
+        _fremove (Callable[[Callable[[object,EventArgs], None]], None] | None): function to be used for removing a callable value.
+        _proxy:"Event" | None: Object to be use as proxy for the descriptor
+    """
+    _fadd: Callable[[Callable[[object,EventArgs], None]], None] | None
+    _fremove:Callable[[Callable[[object,EventArgs], None]], None] | None
+    _proxy: Union["Event" , None]
+
+    def __init__(
+        self,
+        fadd:Callable[[Callable[[object,EventArgs], None]], None] | None = None,
+        fremove:Callable[[Callable[[object,EventArgs], None]], None] | None = None,
+    )->None:
+        """
+        event constructor.
+
+        Parameters:
+            fadd (Callable[[Callable[[object,EventArgs], None]], None] | None): function to be used for adding a callable value.
+            fremove (Callable[[Callable[[object,EventArgs], None]], None] | None): function to be used for removing a callable value.
+        Return:
+            None
+        """
+        self._fadd = fadd
+        self._fremove = fremove
+        self._proxy = None
+
+    
+    def add(self,fadd:Callable[[Callable[[object,EventArgs], None]], None])->"event":
+        """
+        Allows provide the function will be use to add a callable.
+
+        Parameters:
+            fadd (Callable[[Callable[[object,EventArgs], None]], None] | None): function to be used for adding a callable value.
+        Return:
+            event: Current instance with the new fadd function
+        """
+        self._fadd = fadd
+        return self
+
+    def remove(self,fremove:Callable[[Callable[[object,EventArgs], None]], None])->"event":
+        """
+        Allows provide the function will be use to remove a callable.
+
+        Parameters:
+            fremove (Callable[[Callable[[object,EventArgs], None]], None] | None): function to be used for removing a callable value.
+        Return:
+            event: Current instance with the new fremove function
+        """
+        self._fremove = fremove
+        return self
+    
+    def __get__(self, instance:object, owner:type)->"Event":
+        """
+        Method to get descriptor value.
+
+        Parameters:
+            instance (object): The instance of the owning class. This parameter
+                is `None` when accessed from the class instead of from the instance.
+            owner (type): The type of the owning class.
+
+        Return:
+            Event: Event value stored in the descriptor.
+        """
+        if self._proxy is None or self._proxy._instance != instance:
+            self._proxy = self.Event(instance, self)
+
+        return self._proxy
+    
+
+    class Event:
+        """
+        Event is class used as proxy for the 'event' descriptor. its responsability is execute _fadd and _fremove when operators += and -- are used over the member marked as @event.
+
+        Attributes:
+            _instance (Any): Stores the object using @event as memeber (the object implementing the event).
+            _event_descriptor (event): Stores the descriptor that is using the instance as proxy.
+        """
+        _instance:Any
+        _event_descriptor:"event"
+
+        def __init__(self, instance:Any, event_descriptor:"event")->None:
+            """
+            Event constructor.
+
+            Parameters:
+                instance (Any): object using @event as memeber (the object implementing the event).
+                event_descriptor (event): descriptor that is going to use this instance as proxy.
+            Return:
+                None
+            """
+            self._instance = instance
+            self._event_descriptor = event_descriptor
+
+        def __iadd__(self, value:Callable[[object,EventArgs], None])->"event.Event":
+            """
+            Executes the _fadd (responsible of describe how the callable sholud be added) passing the callable as parameter.
+
+            Parameters:
+                value (Callable[[object,EventArgs], None]): callable to be passed as parammeter to _fadd.
+
+            Return:
+                event.Event: Current instance.
+            """
+            self._event_descriptor._fadd(self._instance, value)
+            return self
+
+        def __isub__(self, value:Callable[[object,EventArgs], None])->"event.Event":
+            """
+            Executes the _fremove (responsible of describe how the callable sholud be removed) passing the callable as parameter.
+
+            Parameters:
+                value (Callable[[object,EventArgs], None]): callable to be passed as parammeter to _fremove.
+
+            Return:
+                event.Event: Current instance.
+            """
+            self._event_descriptor._fremove(self._instance, value)
+            return self
+    
+ 
+#staticproperty
+
---
+++ test.py
@@ -0,0 +1,199 @@
+from python_sharp import *
+
+class Point:
+    def __init__(self,x,y):
+        self.x =x
+        self.y =y 
+
+    @property
+    def X(self):
+        return self.x
+    
+    @property
+    def Y(self):
+        return self.y
+
+    def __sub__(self,value):
+        if isinstance(value, Point):
+            return Vector(self.x - value.x, self.y - value.y)
+        return NotImplemented
+
+    def __str__(self):
+        return "(%d, %d)" % (self.X,self.Y)
+    
+class Vector:
+    def __init__(self,i,j):
+        self.i =i
+        self.j =j 
+
+    @property
+    def I(self):
+        return self.i
+    
+    @property
+    def J(self):
+        return self.j 
+
+    def __str__(self):
+        return "%d i, %d j" % (self.I,self.J)
+
+
+
+class LocationChangedEventArgs(EventArgs):
+    
+    def __init__(self,delta:Vector):
+        super().__init__()
+        self.delta = delta
+
+    @property
+    def Delta(self)->Vector:
+        return self.delta
+
+class LocationChangingEventArgs(CancellableEventArg):
+    def __init__(self,location:Point):
+        super().__init__()
+        self.location = location
+
+    @property
+    def Location(self)->Vector:
+        return self.location
+
+
+
+
+class Person:
+    def __init__(self,name):
+        self.name = name
+        self.location = Point(0,0)
+        self.nameChangedcalbacks = Delegate()
+        self.locationChangedcallbacks = Delegate()
+        self.locationChangingcallbacks = Delegate()
+        
+
+# region Properties
+
+
+    @property
+    def Name(self): 
+        return self.name
+               
+    @Name.setter 
+    def Name(self,value:str):
+        self.name = value
+        self._OnNameChanged(EventArgs()) 
+
+     
+    @property
+    def Location(self):
+        return self.location
+    
+    @Location.setter
+    def Location(self,value:Point)->None:
+        
+        locationEventArgs = LocationChangingEventArgs(value)
+        self._OnLocationChanging(locationEventArgs)
+        
+        if(not locationEventArgs.Cancel):
+            previous = self.location 
+            self.location = value
+            self._OnLocationChanged(LocationChangedEventArgs(self.location - previous))
+
+# endregion
+
+# region  Methods
+   
+    def _OnNameChanged(self,e:EventArgs)->None:
+        self.nameChangedcalbacks(self,e)
+    
+    def _OnLocationChanging(self,e:LocationChangingEventArgs)->None:
+        self.locationChangingcallbacks(self,e)
+
+    def _OnLocationChanged(self,e:LocationChangedEventArgs)->None:
+        self.locationChangedcallbacks(self,e)          
+        
+# endregion
+
+# region Events
+    
+    @event
+    def NameChanged(self,value: Callable[[object, EventArgs], None] ):
+        self.nameChangedcalbacks += value 
+        
+    @NameChanged.remove
+    def NameChanged(self,value: Callable[[object, EventArgs], None]):
+       self.nameChangedcalbacks -= value
+
+
+    @event
+    def LocationChanging(self,value:Callable[[object, LocationChangedEventArgs], None]):
+        self.locationChangingcallbacks += value
+    
+    @LocationChanging.remove
+    def LocationChanging(self,value:Callable[[object, LocationChangedEventArgs], None]):
+       self.locationChangingcallbacks -= value
+                     
+    
+    @event      
+    def LocationChanged(self,value:Callable[[object, LocationChangingEventArgs], None]):
+        self.locationChangedcallbacks += value
+    
+    @LocationChanged.remove
+    def LocationChanged(self,value:Callable[[object, LocationChangingEventArgs], None]):
+       self.locationChangedcallbacks -= value  
+
+    
+# endregion
+
+
+    
+
+#use of it
+
+p = Person("Carlos")
+
+def callback_function(sender:object,e:EventArgs):
+    print("callback sender %s, Eventargs: %s" % (sender,e))
+    print("Person name %s" % sender.Name)
+
+
+class School:
+    def __init__(self):
+        pass
+
+    def persona_nameChanged(self,sender:object,e:EventArgs)->None:
+        print("School just signed up to %s"% sender.Name)
+
+    def persona_locationchanged(self,sender:object,e:LocationChangedEventArgs)->None:
+        print("Person %s change its localitation by %s units" % (sender.Name,e.Delta))
+
+    def persona_locationchanging(self,sender:object,e:LocationChangingEventArgs)->None:
+        if e.Location.X > 100:
+            print("Person %s can't be changed that far, 'X can't be greater than 100. value: %d'" % (sender.Name,e.Location.X))
+            e.Cancel = True
+
+e = School()
+p.NameChanged += e.persona_nameChanged 
+p.NameChanged += callback_function
+p.Name = "Susa"
+p.NameChanged -= callback_function
+p.Name = "Nos"
+p.Location = Point(5,5)
+p.LocationChanged += e.persona_locationchanged
+p.Location = Point(15,15)
+p.LocationChanged -= e.persona_locationchanged 
+p.Location = Point(30,30)
+p.LocationChanging += e.persona_locationchanging
+
+p.Location = Point(13,13)
+p.Location = Point(115,2)
+
+print("%s location at %s" %(p.Name, p.Location))
+
+########################################################
+
+
+#Asignacion de un delegado en lugar de un metodo directamente a un evento plus pasando un metodo Eventargs a un evento LocationEventargs
+d = Delegate(callback_function) 
+p.LocationChanged += d
+p.Name = "Chisus"
+######################################################################################################################################
